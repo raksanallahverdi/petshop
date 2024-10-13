c9892b0a6d990d1704ef4965af61c7e6ea92d955
raksanallahverdi <raksanama@code.edu.az>
2024-10-14 00:26:17 +0400

    json-server deployed

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL="http://localhost:3000";
+export const API_BASE_URL="https://json-application.vercel.app";
 export const endpoints={
     blogs:"/blogs",
     users:"/users",
