5e9a53e69d980aa24dad223ff28295e03c035638
raksanallahverdi <raksanama@code.edu.az>
2024-10-14 18:52:27 +0400

    Responsive css

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,6 +5,8 @@ import { addData } from "./helpers.js";
 let localUsers = JSON.parse(localStorage.getItem("user")) || [];
 import { API_BASE_URL } from "./constants.js"
 const registerForm = document.querySelector(".registerForm")
+const registerFormResponsive = document.querySelector(".registerFormResponsive")
+const loginFormResponsive = document.querySelector(".loginFormResponsive")
 const loginForm = document.querySelector(".loginForm")
 const loginLink = document.querySelector('.loginLink');
 const favoritesLink = document.querySelector('.favoritesLink');
@@ -20,14 +22,11 @@ const logOutLink=document.querySelector(".logOutLink");
 window.addEventListener('load', async () => {
     const isLogged = await checkUser();
     console.log('Logged: ', isLogged);
-   console.log(loginLink);
    
-   // Check if there are any users in localStorage
    const users = JSON.parse(localStorage.getItem('user')) || [];
    console.log(users);
    
 
-   // If no users exist, display login and register links
    if (users.length == 0) {
     loginLink.style.display = 'inline-block'; 
        if (respLogin)  respLogin.style.display = 'inline-block'; 
@@ -125,22 +124,22 @@ registerForm?.addEventListener("submit", async (e) => {
         }
     }
     if(!newPassword || !newUsername || !newEmail  ){
-        alert("Empty Input is not allowed!");
+        Swal.fire("Empty Input is not allowed!");
         return
     }
     if (duplicateUsername) {
-        alert("Username already exist");
+        Swal.fire("Username already exist");
         return
     }
     if (duplicateEmail) {
-        alert("Email already exist");
+        Swal.fire("Email already exist");
         return
     }
    
     const passwordRegex =
     /(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9!@#$%^&*]{5,30})$/;
   if (!passwordRegex.test(newPassword)) {
-   alert("Password does NOT match requirements!")
+    Swal.fire("Password does NOT match requirements!");
     return;
   }
 
@@ -159,33 +158,147 @@ registerForm?.addEventListener("submit", async (e) => {
         console.error("Error during registration:", error);
         alert("There was an error registering the user. Please try again.");
     }
-    
-
-
 })
-
-loginForm?.addEventListener("submit",async(e)=>{
+registerFormResponsive?.addEventListener("submit", async (e) => {
     e.preventDefault();
-   
-    
-    
+    console.log(e.target.children[3]);
     const users = await getAllData(API_BASE_URL, endpoints.users);
-    const username= e.target.children[3].value;
-    const password=e.target.children[4].value;
 
-    const check=users.find((user)=>user.username==username)
-    if (check && check.password==password){
-       console.log("Successfully logged in");
-       localStorage.setItem("user", JSON.stringify(check.id));
-       window.location.replace("index.html");
-       
+    const newUsername = e.target.children[3].value;
+    const newEmail = e.target.children[4].value;
+    const newPassword = e.target.children[5].value;
+
+    const duplicateUsername = checkUserName(users, newUsername);
+    const duplicateEmail = checkEmail(users, newEmail);
+
+
+    function checkUserName(users, username) {
+        console.log("Users Array", users);
+        console.log(username);
+        console.log( users[0].username);
+        const check = users.find((x) => x.username == username);
+        if (check) {
+            console.log("Found matching username:", check);
+            return true; // Return true if a match is found
+        } else {
+            console.log("No matching username found.");
+            return false; // Return false if no match found
+        }
     }
-    else{
-        console.log("Username or Password incorrect");
 
+    function checkEmail(users, email) {
+        console.log("Users Array", users);
+        const check = users.find((x) => x.email == email);
+        if (check) {
+            console.log("Found matching email:", check);
+            return true; // Return true if a match is found
+        } else {
+            console.log("No matching email found.");
+            return false; // Return false if no match found
+        }
     }
+    if(!newPassword || !newUsername || !newEmail  ){
+        Swal.fire("Empty Input is not allowed!");
+        return
+    }
+    if (duplicateUsername) {
+        Swal.fire("Username already exist");
+        return
+    }
+    if (duplicateEmail) {
+        Swal.fire("Email already exist");
+        return
+    }
+   
+    const passwordRegex =
+    /(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9!@#$%^&*]{5,30})$/;
+  if (!passwordRegex.test(newPassword)) {
+    Swal.fire("Password does NOT match requirements!");
+    return;
+  }
 
+    const newUser={       
+        username:newUsername,
+        email:newEmail,
+        password:newPassword,
+    }
+  
+    try {
+        await addData(API_BASE_URL, endpoints.users, newUser);
+    
+            window.location = "http://127.0.0.1:5500/login.html";
+
+    } catch (error) {
+        console.error("Error during registration:", error);
+        alert("There was an error registering the user. Please try again.");
+    }
 })
+console.log(loginForm.querySelector('input[type="text"]'));
+console.log(loginForm.querySelector('input[type="password"]'));
+
+console.log("Login form:", loginForm);
+if (loginForm) {
+    loginForm.addEventListener('submit',async(e)=>{
+        e.preventDefault();
+        console.log("salam");
+        
+        
+        const users = await getAllData(API_BASE_URL, endpoints.users);
+        const username = e.target.querySelector('input[type="text"]').value;
+    const password = e.target.querySelector('input[type="password"]').value;
+    
+        console.log(username);
+        console.log(password);
+        
+        const check=users.find((user)=>user.username==username)
+        if (check && check.password==password){
+           console.log("Successfully logged in");
+           localStorage.setItem("user", JSON.stringify(check.id));
+           window.location.replace("index.html");
+           
+        }
+        else{
+            Swal.fire("Username or Password incorrect");
+    
+        }
+    
+    })
+}
+ else{
+    console.log("Login form not found");
+
+ }
+ if (loginFormResponsive) {
+    loginFormResponsive.addEventListener('submit',async(e)=>{
+        e.preventDefault();
+        console.log("salam");
+        
+        
+        const users = await getAllData(API_BASE_URL, endpoints.users);
+        const username = e.target.querySelector('input[type="text"]').value;
+    const password = e.target.querySelector('input[type="password"]').value;
+    
+        console.log(username);
+        console.log(password);
+        
+        const check=users.find((user)=>user.username==username)
+        if (check && check.password==password){
+           console.log("Successfully logged in");
+           localStorage.setItem("user", JSON.stringify(check.id));
+           window.location.replace("index.html");
+           
+        }
+        else{
+            Swal.fire("Username or Password incorrect");
+    
+        }
+    
+    })
+}
+ else{
+    console.log("Login form not found");
+
+ }
 
 
 const signUpButton = document.getElementById('signUp');
